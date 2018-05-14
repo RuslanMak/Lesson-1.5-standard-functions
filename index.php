1a59fce55bcd1cc4dc2b77ce4d557cd4f691ad65
RuslanMak <makarovemd@gmail.com>
2018-05-14 09:00:17 +0300

    version-2

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 $json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=48ca7ca21381462c0cee89238cc15fc8');
 $data = json_decode($json, true);
 
+$arrayWeather = [$data];
+$temperatureC = round($data['main']['temp'], 1) - 273;
 ?>
 
 <html>
@@ -10,17 +12,27 @@ $data = json_decode($json, true);
 </head>
 
 <body>
-    <h1><?php echo $data['name'] ?></h1>
-    <div></div>
-    <img src="http://openweathermap.org/img/w/<?php echo $data['weather']['0']['icon'] ?>.png">
-    <p><b>Temperature: </b><?php echo round($data['main']['temp'], 1) - 273 ?>&#176</p>
-    <p><b>Weather: </b><?php echo $data['weather']['0']['main'] ?></p>
-    <p><b>Sky: </b><?php echo $data['weather']['0']['description'] ?></p>
-    <p><b>Humidity: </b><?php echo $data['main']['humidity'] ?>%</p>
-    <p><b>Wind speed: </b><?php echo $data['wind']['speed'] ?> meter/sec</p>
-    <p><b>Cloudiness: </b><?php echo $data['clouds']['all'] ?>%</p>
-    <p><b>Atmospheric pressure: </b><?php echo $data['main']['pressure'] ?> hPa</p>
+    <?php if($data) : ?>
+        <?php foreach ($arrayWeather as $data): ?>
+            <h1><?php echo $data['name'] ?></h1>
+            <div></div>
+            <?php echo "<img src=\"http://openweathermap.org/img/w/" . $data['weather']['0']['icon'] . ".png\">" . "<br>" ?>
+            <?php echo "<b>Temperature: </b>" . $temperatureC . "&#176" . "<br>" ?>
+            <?php echo "<b>Weather: </b>" . $data['weather']['0']['main'] . "<br>" ?>
+            <?php echo "<b>Sky: </b>" . $data['weather']['0']['description'] . "<br>" ?>
+            <?php echo "<b>Humidity: </b>" . $data['main']['humidity'] . " %" . "<br>" ?>
+            <?php echo "<b>Wind speed: </b>" . $data['wind']['speed'] . " meter/sec" . "<br>" ?>
+            <?php echo "<b>Cloudiness: </b>" . $data['clouds']['all'] . "%" ?>
+            <?php echo "<b>Atmospheric pressure: </b>" . $data['main']['pressure'] . " hPa" . "<br>" ?>
+        <?php endforeach; ?>
+
+    <?php else : ?>
+        <?php echo "Сервер не доступен!" ?>
+    <?php endif; ?>
 </body>
 </html>
+
+
+
 
 
